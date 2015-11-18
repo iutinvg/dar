326c353b6fa202286cc74bcd0e8fee9cc3007eb2
Viacheslav Iutin <iutinvg@gmail.com>
2015-11-18 10:26:30 +0600

    mdb

--- db/memory.py
+++ db/memory.py
@@ -1,8 +1,26 @@
+import hashlib
+import uuid
+
 from db.base import BaseDb
+from db.document import Document
 
 
 class MemoryDb(BaseDb):
     """docstring for MemoryDB"""
     def __init__(self, name):
         super(MemoryDb, self).__init__(name)
-        self._data = {}
+        self.data = {}
+
+    def post(self, value):
+        uid = hashlib.sha1(str(uuid.uuid4())).hexdigest()
+        document = Document(value)
+        self.data[uid] = document
+        return document
+
+    def put(self, uid, value):
+        document = self.data[uid]
+        document.update(value)
+        return document
+
+    def get(self, uid, rev=None):
+        return self.data[uid].value(rev)
